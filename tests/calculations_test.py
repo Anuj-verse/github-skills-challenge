40e5b5c88496780831ece9bfcfafeda3767da564
ANUJ GOPE <anujfw1234@gmail.com>
2026-09-21 06:06:36 +0000

    Fix tests and raise coverage

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -34,6 +34,11 @@ def test_area_of_circle_zero_radius():
     assert result == 0
 
 
+def test_area_of_circle_negative_radius():
+    with pytest.raises(ValueError):
+        area_of_circle(-1)
+
+
 def test_get_nth_fibonacci_zero():
     """Test with n=0."""
     # Arrange
@@ -58,6 +63,11 @@ def test_get_nth_fibonacci_one():
     assert result == 1
 
 
+def test_get_nth_fibonacci_negative():
+    with pytest.raises(ValueError):
+        get_nth_fibonacci(-1)
+
+
 def test_get_nth_fibonacci_ten():
     """Test with n=10."""
     # Arrange
@@ -67,4 +77,4 @@ def test_get_nth_fibonacci_ten():
     result = get_nth_fibonacci(n)
 
     # Assert
-    assert result == 89
+    assert result == 55
